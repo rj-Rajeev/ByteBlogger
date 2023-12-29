0353a57bd90d4d15d12e2edab05d25c0257086de
Rajeev Bhardwaj <officialrajeevbhardwaj@gmail.com>
2023-12-29 12:04:14 +0530

    setup redux toolkit Store and add Tailwind CSS to Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 
+import { Outlet } from 'react-router-dom';
 import './App.css'
+import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
 
 function App() {
   console.log();
 
   return (
     <>
-      <h1>I am App.JSX</h1>
-
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
     </>
   )
 }
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <h1 className='bg-black text-white pt-4 font-extrabold py-5 text-center'>I am ByteBlogger</h1>
+  )
+}
+
+export default Header
